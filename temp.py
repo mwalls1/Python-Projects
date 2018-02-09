0764d56c2363b33e8dd3ce01eacc29faff9a6b3c
mwalls1 <walls594@gmail.com>
2018-02-09 12:25:33 -0600

    Fixed some issues

---
+++ temp.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a = "Git is ba in"
+print(a)
---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 12:21:51 2018
+
+@author: Mason
+"""
+a = 50
+b = 50
+c = a-b+a
+print(c)
